a6e20e837bc80d0edec2b8470288beb83cbf3c00
Marcin Popławski <marpop8@gmail.com>
2019-03-03 23:55:39 +0100

    Get keywords from page content

--- app/keywords.py
+++ app/keywords.py
@@ -1,6 +1,16 @@
 from urllib.request import urlopen
 
+from bs4 import BeautifulSoup
+
 
 def get_page_content(url):
     page = urlopen(url)
     return str(page.read())
+
+
+def get_keywords(page_content):
+    soup = BeautifulSoup(page_content, "html.parser")
+    meta_keywords_tag = soup.find(name="meta", attrs={"name": "keywords"})
+    keywords_string = meta_keywords_tag.attrs.get("content")
+    keywords_list = keywords_string.split()
+    return set(keywords_list)
--- app/tests/test_keywords.py
+++ app/tests/test_keywords.py
@@ -1,7 +1,11 @@
-from app.keywords import get_page_content
+from app.keywords import get_keywords, get_page_content
 
 
 def test_get_page():
     url = "https://www.python.org"
     assert "head" in get_page_content(url)
     assert "body" in get_page_content(url)
+
+
+def test_get_keywords(python_html):
+    assert get_keywords(python_html) == {"Python", "programming", "language"}
