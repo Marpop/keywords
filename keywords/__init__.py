465d4e63438dc0e2783586f365310ad88fa8902c
Marcin Popławski <marpop8@gmail.com>
2019-03-04 04:39:07 +0100

    Fix missing SECRET_KEY

--- keywords/__init__.py
+++ keywords/__init__.py
@@ -1,5 +1,12 @@
+import os
+
 from flask import Flask
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 import keywords.views  # noqa: E402 F401 isort:skip
