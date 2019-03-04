74c7b298acea12986461520197d277ed9a79b254
Marcin Popławski <marpop8@gmail.com>
2019-03-04 05:10:23 +0100

    Add flask bootstrap

--- keywords/__init__.py
+++ keywords/__init__.py
@@ -3,10 +3,12 @@ import os
 from flask import Flask
 
 from dotenv import load_dotenv
+from flask_bootstrap import Bootstrap
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+bootstrap = Bootstrap(app)
 
 import keywords.views  # noqa: E402 F401 isort:skip
--- keywords/forms.py
+++ keywords/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, validators
+from wtforms import StringField, SubmitField, validators
 
 
 class URLForm(FlaskForm):
     url = StringField("URL:", validators=[validators.URL(), validators.DataRequired()])
+    submit_button = SubmitField("Submit")
